a0fa8b3b5a1a122b6ad3776e68079be8b7ff9b7f
Jai singh <jaibhandari804@gmail.com>
2022-09-29 18:05:31 +0530

    file stack

--- Stack/Reverse_string.java
+++ Stack/Reverse_string.java
@@ -1,20 +1,22 @@
+import java.util.*;
+public class Reverse_string {
+    
     public String reverse(String S){
 
-       StringBuilder sb=new StringBuilder();
-       Stack<Character> st=new Stack<Character>();
-       
-       for(int i=0;i<S.length();i++)
-       {
-           st.push(S.charAt(i));
-       }
-       
-       for(int i=0;i<S.length();i++)
-       {
-           sb.append(st.pop());
-       }
-       return sb.toString();
-    }
-
-
-
+        StringBuilder sb=new StringBuilder();
+        Stack<Character> st=new Stack<Character>();
+        
+        for(int i=0;i<S.length();i++)
+        {
+            st.push(S.charAt(i));
+        }
+        
+        for(int i=0;i<S.length();i++)
+        {
+            sb.append(st.pop());
+        }
+        return sb.toString();
+     } 
+    
+}
 
